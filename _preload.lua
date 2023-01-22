d255e4678fcec3f44d6f037f6cc0f76912446be5
MarcasRealAccount <thetremine.lumicks@gmail.com>
2023-01-22 11:08:51 +0100

    Bump patch version

--- _preload.lua
+++ _preload.lua
@@ -3,7 +3,7 @@ local api = p.api
 
 p.extensions     = p.extensions or {}
 p.extensions.pkg = {
-	_VERSION     = "1.3.0",
+	_VERSION     = "1.3.1",
 	dir          = common:scriptDir(),
 	reloadRepos  = true,
 	repos        = {
