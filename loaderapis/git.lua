ff3b0afe1569f6b853425b938da2114202f2bee1
MarcasRealAccount <thetremine.lumicks@gmail.com>
2023-01-22 10:34:27 +0100

    Fix git loader failing

--- loaderapis/git.lua
+++ loaderapis/git.lua
@@ -31,18 +31,18 @@ function git:loadPackage(repo, pack, version, filepath)
 		end
 		
 		if not os.executef("git clone %s \"%s\" \"%s\"", gitBranch, filepath, version.fullPath) then
-			common.fail("Failed to clone package '%s' version '%s'", pack.name, pkg:semverToString(version.version))
+			common:fail("Failed to clone package '%s' version '%s'", pack.name, pkg:semverToString(version.version))
 			return
 		end
 		
 		if version.commit and not os.executef("git -C %q checkout %s", version.fullPath, version.commit) then
-			common.fail("Failed to checkout commit '%s' package '%s' version '%s'", verison.commit, pack.name, pkg:semverToString(version.version))
+			common:fail("Failed to checkout commit '%s' package '%s' version '%s'", verison.commit, pack.name, pkg:semverToString(version.version))
 			return
 		end
 		
 		if version.apply_patch then
 			if not os.executef("git -C \"%s\" am -q --no-gpg-sign \"%s/patches/%s-%s-%s.patch\"", version.fullPath, repo.dir, prefix, pack.name, pkg:semverToString(version.version)) then
-				common.fail("Failed to apply patch for package '%s' version '%s'", pack.name, pkg:semverToString(version.version))
+				common:fail("Failed to apply patch for package '%s' version '%s'", pack.name, pkg:semverToString(version.version))
 				return
 			end
 			io.writefile(string.format("%s/%s", version.fullPath, ".pkgpatchversion"), tostring(version.patch_version))
